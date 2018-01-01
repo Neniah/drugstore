8ed4e3c4d6e24c802026ad951cc233f63a781cc7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-01 13:42:23 +0100

    searching

---
+++ new.php
@@ -0,0 +1,12 @@
+
+
+
+
+<form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+    <input name="name" type="text" placeholder="Nombre..." autofocus >
+    <input name="composition" type="text" placeholder="Composición..." autofocus >
+    <input name="quantity" type="text" placeholder="Cantidad..." autofocus >
+    <input name="brand" type="text" placeholder="Marca..." autofocus >
+
+    <input type="submit" name="Guardar" class="" value="Guardar">
+</form>
