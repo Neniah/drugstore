625cffa99fc6e14fdd5768a341d4b658a2aa018d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-03 17:06:21 +0100

    some style

--- index.php
+++ index.php
@@ -1,61 +1,84 @@
+
+
+
 <?php
-$con = @mysqli_connect('localhost', 'maria', 'maria', 'drugstore');
+  $con = @mysqli_connect('localhost', 'maria', 'maria', 'drugstore');
+  if (!$con) {
+      echo "Error: " . mysqli_connect_error();
+  	exit();
+  } else{
 
-if (!$con) {
-    echo "Error: " . mysqli_connect_error();
-	exit();
-}
-echo 'Connected to MySQL';
-
-
-// table
-echo '<table>
-        <thead>
-            <tr>
-                <th>NOMBRE</th>
-                <th>COMPOSICION</th>
-                <th>CANTIDAD</th>
-                <th>FECHA</th>
-            </tr>
-        </thead>
-        <tbody>';
-
-
-// Some Query
-$sql 	= 'SELECT * FROM homeo';
-$query 	= mysqli_query($con, $sql);
-while ($row = mysqli_fetch_array($query))
-{
-	echo '<tr>';
-  echo '<td>'.$row['name'].'</td>';
-  echo '<td>'.$row['composition'].'</td>';
-  echo '<td>'.$row['quantity'].'</td>';
-  echo '<td>'.$row['updated'].'</td>';
-
-  echo '</tr>';
-/*
-  echo '<tr>
-            <td>'.$row['name'].'</td>
-            <td>'.$row['composition'].'</td>
-            <td>'.$row['quantity'].'</td>
-            <td>'.$row['updated']'</td>
-        </tr>';
-*/
-}
-
-// Close connection
-mysqli_close ($con);
-
- ?>
+  }
+//echo "Connected to database";
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>
-      Homeo
-    </title>
+    <title>Homeo</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body>
-    <h1>Homeo</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Homeopatía</a>
+  <div class="" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="nuevo.php">Nuevo</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <div class="container">
+
+
+      <br>
+      <form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+          <input id="buscar" name="buscar" type="search" placeholder="Buscar aquí..." autofocus >
+          <input type="submit" name="buscador" class="boton peque aceptar" value="buscar">
+      </form>
+      <br>
+
+      <?php
+      if($_POST){
+
+        $busqueda = trim($_POST['buscar']);
+
+        $con = @mysqli_connect('localhost', 'maria', 'maria', 'drugstore');
+
+        if (!$con) {
+            echo "Error: " . mysqli_connect_error();
+        	exit();
+        }
+
+        $sql = "SELECT * FROM homeo WHERE name LIKE '%" .$busqueda. "%' ORDER BY name";
+        $query 	= mysqli_query($con, $sql);
+        // table
+        echo '<table class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th>NOMBRE</th>
+                        <th>COMPOSICION</th>
+                        <th>CANTIDAD</th>
+                        <th>FECHA</th>
+                    </tr>
+                </thead>
+                <tbody>';
+        while ($row = mysqli_fetch_array($query))
+        {
+          echo '<tr>';
+          echo '<td>'.$row['name'].'</td>';
+          echo '<td>'.$row['composition'].'</td>';
+          echo '<td>'.$row['quantity'].'</td>';
+          echo '<td>'.$row['updated'].'</td>';
+          echo '</tr>';
+        }
+
+      }
+       ?>
+      <div class="col-md-8">
+
+      </div>
+    </div>
   </body>
 </html>
--- nuevo.php
+++ nuevo.php
@@ -30,13 +30,46 @@ if($_POST){
 }
  ?>
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Nuevo
+    </title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row">
+            div.
+          </div>
+          <form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+            <div class="form-group">
+              <label >Nombre</label>
+              <input class="form-control" name="name" type="text" placeholder="Nombre..." autofocus >
+            </div>
+            <div class="form-group">
+              <label >Composición</label>
+              <input class="form-control" name="composition" type="text" placeholder="Composición..." autofocus >
+            </div>
+            <div class="form-group">
+              <label >Cantidad</label>
+              <input class="form-control" name="quantity" type="text" placeholder="Cantidad..." autofocus >
+            </div>
+            <div class="form-group">
+              <label >Marca</label>
+              <input class="form-control" name="brand" type="text" placeholder="Marca..." autofocus >
+            </div>
 
+              <input type="submit" name="Guardar" class="btn btn-success" value="Guardar">
+              <a href="index.php" class="btn btn-danger">Cancelar</a>
+          </form>
 
-<form id="buscador" name="buscador" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-    <input name="name" type="text" placeholder="Nombre..." autofocus >
-    <input name="composition" type="text" placeholder="Composición..." autofocus >
-    <input name="quantity" type="text" placeholder="Cantidad..." autofocus >
-    <input name="brand" type="text" placeholder="Marca..." autofocus >
-
-    <input type="submit" name="Guardar" class="" value="Guardar">
-</form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
